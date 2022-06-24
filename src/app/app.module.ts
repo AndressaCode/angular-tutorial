30f8e63a3b1cb3c13de44e50963f370d245cf806
Andressa Silva <andressaszx@gmail.com>
2022-06-23 22:21:35 -0300

    styling the extrato-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
-import { NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -6,6 +7,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExtratoComponent } from './extrato/extrato.component';
 import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
+import localePt from '@angular/common/locales/pt';
+
+
+registerLocaleData(localePt, 'pt');
 
 @NgModule({
   declarations: [
@@ -18,7 +23,11 @@ import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferen
     AppRoutingModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'pt' },
+    {provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' },
+
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
