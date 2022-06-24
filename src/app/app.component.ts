034780faeb52a03905a8dc7a0f0a3046f27575cf
Andressa Silva <andressaszx@gmail.com>
2022-06-23 21:30:44 -0300

    iterando lista com ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-tutorial';
+  title = 'bytebank';
+  transferencias: any[] = [];
 
-  destino: number;
-  valor: number;
-
-  transferir($event){
+  transferir($event) {
     console.log($event);
-    this.destino = $event.destino;
-    this.valor = $event.valor;
+    this.transferencias.push($event);
   }
 }
